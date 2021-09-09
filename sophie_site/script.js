47d40e3efde06ddb2e3734155406568c2e073900
Xiao-Peng-Sophie <xp2022@nyu.edu>
2021-09-09 19:38:54 +0800

    。。。

--- sophie_site/script.js
+++ sophie_site/script.js
@@ -2,7 +2,7 @@
 let icon1=document.getElementById("icon01");
 let icon2=document.getElementById("icon02");
 icon1.addEventListener("mouseover",()=>{
-  icon1.style.transform="scale(1.3) rotate(360deg)";
+  icon1.style.transform="rotate(360deg) scale(1.3)";
 
 });
 
@@ -11,7 +11,7 @@ icon1.addEventListener("mouseout",()=>{
 });
 
 icon2.addEventListener("mouseover",()=>{
-  icon2.style.transform="scale(1.3) rotate(360deg)";
+  icon2.style.transform="rotate(360deg) scale(1.3) ";
 
 });
 
