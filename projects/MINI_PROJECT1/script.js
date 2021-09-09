f92d4126aeeeea1f5377c4e1b0e968e3b6b68421
Xiao-Peng-Sophie <xp2022@nyu.edu>
2021-09-09 14:19:21 +0800

    999

--- projects/MINI_PROJECT1/script.js
+++ projects/MINI_PROJECT1/script.js
@@ -2,7 +2,7 @@ let words=document.getElementById("content");
 let symbol=document.getElementById("image");
 
 let w=words.innerHTML;
-//let angle=45;
+
 
 let letters=words.innerHTML.split("");
 let containers=letters.map((l)=>{
@@ -23,11 +23,7 @@ let yPositions=letters.map((l)=>{
 words.innerHTML=containers.join(" ");
 let split_words=words.getElementsByTagName("span");
 
-// split_words.forEach(function(word){
-//   word.addEventListener("mouseover",()=>{
-//     word.style.color="red";
-//   });
-// });
+
 let images=[];
 for(let j=0;j<5;j+=1){
   images[j]="<img>"+symbol+"</img>";
@@ -44,7 +40,7 @@ symbol.addEventListener("mouseout",()=>{
 
 function changeColor(word,angle,size,y){
   word.addEventListener("mouseover",()=>{
-  //  word.style.color="pink";
+
   if(Math.random()<0.5){
     angle=-angle;
   }
@@ -56,59 +52,13 @@ function changeColor(word,angle,size,y){
     y=y/2;
   }
 
-  // word.style.color="pink"
-    //word.style.display="inline-block";
-    //word.style.fontSize=Number(size)+"px";
+
 
     word.style.transform="rotate("+Number(angle)+"deg) translate(0,"+Number(y)*2+"px)";
-    //word.style.transform= "translate(0,"+Number(y)+"px)";
-
-    //word.style.transform="rotate(90deg)";
+  
   });
 }
 
 for(let i=0;i<split_words.length;i+=1){
   changeColor(split_words[i],angles[i],sizes[i],yPositions[i]);
 }
-
-// control.addEventListener("input",()=>{
-//   let sliderValue=control.value;
-//   //let letterSize=Number(sliderValue)/2+20;
-//   // words.style.fontSize=letterSize+"px";
-//   // words.style.transform="rotate("+sliderValue+"deg)";
-//
-//   for(let i=0;i<split_words.length;i+=1){
-//     let ls=Number(sliderValue)*Number(randomness[i])+20;
-//     let c=ls*2;
-//     let alpha=ls/100;
-//
-//     split_words[i].style.transform="rotate(45deg)";//doesn't work even if I change it to a constant
-//     split_words[i].style.fontSize=ls+"px";//works
-//     //split_words[i].style.color= "rgba(0,0,0"+alpha+")";// transparency doesn't work
-//     //"rgba(0,0,0,"+ls/2+")"
-//
-//   }
-//   //words.style.transform="rotate("+ sliderValue+"deg)";
-//
-//
-// });
-
-//"rotate("+ls+"deg)"
-
-//words.style.transform="rotate(35deg)";
-
-//words.style.transform="rotate(90deg)";
-// words.addEventListener("mouseover",()=>{
-//   words.style.color="red";
-//   words.style.fontSize="25px";
-//  words.style.transform="rotate("+sliderValue+"deg)";
-
-//});
-// words.addEventListener("mouseout",()=>{
-//   words.style.color="blue";
-//   words.style.fontSize="20px";
-  //words.style.transform="rotate(0deg)";
-  //words.style.transform="rotate("+sliderValue+"deg)";
-
-
-//});
