bfb1da44858f22b422d6b8c6135549880df10cb0
Xiao-Peng-Sophie <xp2022@nyu.edu>
2021-09-11 16:04:46 +0800

    updated_gallery

---
+++ projects/class2/script.js
@@ -0,0 +1,4 @@
+let button=document.getElementById("button");
+button.addEventListener("click",()=>{
+    window.open("window/index.html", "", "width=200,height=200");
+});
--- projects/rotate?/script.js
+++ projects/rotate?/script.js
@@ -1,4 +1,3 @@
-
 let s1=document.getElementById('snake1');
 let s2=document.getElementById('snake2');
 let s3=document.getElementById('snake3');
@@ -9,21 +8,17 @@ let s7=document.getElementById('snake7');
 let s8=document.getElementById('snake8');
 let s9=document.getElementById('snake9');
 let s10=document.getElementById('snake10');
-let c2=document.getElementById("smallCircle");
 
 s1.addEventListener("mouseover",()=>{
-  s1.style.transform="rotateY(180deg) scale(1.2)";
-  
+  s1.style.transform="rotate(30deg) scale(1.2)";
 });
 
 s1.addEventListener("mouseout",()=>{
   s1.style.transform="rotate(0deg) scale(1)";
-
-
 });
 
 s2.addEventListener("mouseover",()=>{
-  s2.style.transform="rotateY(180deg) scale(1.2)";
+  s2.style.transform="rotate(30deg) scale(1.2)";
 });
 
 s2.addEventListener("mouseout",()=>{
@@ -32,7 +27,7 @@ s2.addEventListener("mouseout",()=>{
 
 s3.addEventListener("mouseover",()=>{
 
-  s3.style.transform="rotate(-180deg) scale(1.2)";
+  s3.style.transform="rotate(30deg) scale(1.2)";
 });
 
 s3.addEventListener("mouseout",()=>{
@@ -45,12 +40,12 @@ s4.addEventListener("mouseover",()=>{
 });
 
 s4.addEventListener("mouseout",()=>{
-  s4.style.transform="rotate(360deg) scale(1)";
+  s4.style.transform="rotate(0deg) scale(1)";
 });
 
 s5.addEventListener("mouseover",()=>{
 
-  s5.style.transform="rotateX(180deg) scale(1.2)";
+  s5.style.transform="rotate(30deg) scale(1.2)";
 });
 
 s5.addEventListener("mouseout",()=>{
@@ -59,7 +54,7 @@ s5.addEventListener("mouseout",()=>{
 
 s6.addEventListener("mouseover",()=>{
 
-  s6.style.transform=" scale(1.2)";
+  s6.style.transform="rotate(30deg) scale(1.2)";
 });
 
 s6.addEventListener("mouseout",()=>{
@@ -68,7 +63,7 @@ s6.addEventListener("mouseout",()=>{
 
 s7.addEventListener("mouseover",()=>{
 
-  s7.style.transform="rotate(360deg) scale(1.2)";
+  s7.style.transform="rotate(30deg) scale(1.2)";
 });
 
 s7.addEventListener("mouseout",()=>{
@@ -78,7 +73,7 @@ s7.addEventListener("mouseout",()=>{
 
 s8.addEventListener("mouseover",()=>{
 
-  s8.style.transform="rotate(90deg) scale(1.2)";
+  s8.style.transform="rotate(30deg) scale(1.2)";
 });
 
 s8.addEventListener("mouseout",()=>{
@@ -87,7 +82,7 @@ s8.addEventListener("mouseout",()=>{
 
 s9.addEventListener("mouseover",()=>{
 
-  s9.style.transform="rotateY(180deg) scale(1.2)";
+  s9.style.transform="rotate(30deg) scale(1.2)";
 });
 
 s9.addEventListener("mouseout",()=>{
@@ -96,7 +91,7 @@ s9.addEventListener("mouseout",()=>{
 
 s10.addEventListener("mouseover",()=>{
 
-  s10.style.transform=" scale(1.2)";
+  s10.style.transform="rotate(30deg) scale(1.2)";
 });
 
 s10.addEventListener("mouseout",()=>{
