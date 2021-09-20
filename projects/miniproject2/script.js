1ee7c4274daaee4d43e373b05b4a571bb6497bf5
Xiao-Peng-Sophie <xp2022@nyu.edu>
2021-09-20 15:58:29 +0800

    👀

--- projects/miniproject2/script.js
+++ projects/miniproject2/script.js
@@ -40,24 +40,24 @@ function open1(){
 
   controller.addEventListener("load", ()=>{
     // open left face 338, 461
-    let leftFace = window.open("leftFace","","width=440, height=600,left=205,top=0"); //****2/3
+    let leftFace = window.open("/abc-student-repo/projects/miniproject2/leftFace","","width=440, height=600,left=205,top=0"); //****2/3
     // send right eye refrence to controller
     controller.mailbox(leftFace);
 
    //293,407
-    let rightFace = window.open("rightFace","","width=380, height=529, left=575,top=40");
+    let rightFace = window.open("/abc-student-repo/projects/miniproject2/rightFace","","width=380, height=529, left=575,top=40");
     controller.mailbox(rightFace);
    //370,222  *1.3
-    let mouth=window.open("mouth","","width=480, height=288, left=350, top=700");
+    let mouth=window.open("/abc-student-repo/projects/miniproject2/mouth","","width=480, height=288, left=350, top=700");
     controller.mailbox(mouth);
    //95,180
-    let nose=window.open("nose","", "width=160, height=230, left=500, top=350");
+    let nose=window.open("/abc-student-repo/projects/miniproject2/nose","", "width=160, height=230, left=500, top=350");
     controller.mailbox(nose);
    //125,125
-    let leftEye=window.open("leftEye","","width=155, height=155, left=400,top=315");
+    let leftEye=window.open("/abc-student-repo/projects/miniproject2/leftEye","","width=155, height=155, left=400,top=315");
     controller.mailbox(leftEye);
 
-    let rightEye=window.open("rightEye","","width=214, height=214, left=600, top=285");
+    let rightEye=window.open("/abc-student-repo/projects/miniproject2/rightEye","","width=214, height=214, left=600, top=285");
     controller.mailbox(rightEye);
 
 
