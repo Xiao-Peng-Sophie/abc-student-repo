48f1f52b56e6e802c9c2d2f19a66c5a813b650b4
Xiao-Peng-Sophie <xp2022@nyu.edu>
2021-09-23 14:01:44 +0800

    mini project3

--- projects/miniproject3/script.js
+++ projects/miniproject3/script.js
@@ -1,8 +1,13 @@
-let moveBox=document.getElementById('movemouse');
-function move(event){
-  let x=event.pageX;
-  let y=event.pageY;
-  moveBox.style.top=y+'px';
-  moveBox.style.left=x+'px';
-}
-moveBox.addEventListener("mousemove",move);
+// let moveBox=document.getElementById('movemouse');
+// function move(event){
+//   let x=event.pageX;
+//   let y=event.pageY;
+//   moveBox.style.top=y+'px';
+//   moveBox.style.left=x+'px';
+// }
+// moveBox.addEventListener("mousemove",move);
+document.addEventListener("mousemove", (e)=>{
+    console.log(e.pageX, e.pageY);
+    document.getElementById("movemouse").style.left = e.pageX-250 +"px";
+    document.getElementById("movemouse").style.top = e.pageY-250 +"px";
+});
